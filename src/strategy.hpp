b0db27794494027b9fd14cadd086a406487465ed
DieSeeKat <lukasanthonissen@gmail.com>
2024-06-04 19:24:23 +0200

    Adds strategy class definitions

---
+++ src/strategy.hpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+// Define return type & parameter types of functors
+// <template typename r, typename parameters...>
+// class Strategy {
+
+    // private:
+        // Hashmap of string to functor
+
+    // public:
+
+        // Register a new strategy to the hashmap
+        // void register_strategy(string name, r (*functor)(parameters...)) {
+        //     // hashmap[name] = functor;
+        // }
+
+        // Unregister a strategy from the hashmap
+
+        // void set_strategy(string name) {
+        //     this.name = name;
+        // }
+
+        // void execute() {
+        //     if name not in map {
+        //         return first entry
+        //     }
+
+        //     return hashmap[name]();
+        // }
+
+        // void execute_by_name(string strategy_name) {
+        //     // Call the functor from the hashmap
+        //     return hashmap[strategy_name]();
+        // }    
+// 
+// }
