abda59a91b059ec26df10a3e0651a64a99c77851
DieSeeKat <lukasanthonissen@gmail.com>
2024-06-08 18:30:08 +0200

    Adds Strategy class

--- src/strategy.hpp
+++ src/strategy.hpp
@@ -1,36 +1,37 @@
 #include <iostream>
+#include <string>
+#include <map>
 
-// Define return type & parameter types of functors
-// <template typename r, typename parameters...>
-// class Strategy {
+template <typename key, typename ReturnType, typename... Args>
+class Strategy
+{
+private:
+    std::map<key, std::function<ReturnType(Args...)>> strategies;
+    key current_strategy;
 
-    // private:
-        // Hashmap of string to functor
+public:
+    void register_strategy(key name, std::function<ReturnType(Args...)> functor)
+    {
+        strategies[name] = functor;
+    }
 
-    // public:
+    void set_strategy(key name)
+    {
+        current_strategy = name;
+    }
 
-        // Register a new strategy to the hashmap
-        // void register_strategy(string name, r (*functor)(parameters...)) {
-        //     // hashmap[name] = functor;
-        // }
+    ReturnType execute(Args... args)
+    {
+        if (strategies.find(current_strategy) == strategies.end())
+        {
+            return strategies.begin()->second(args...);
+        }
 
-        // Unregister a strategy from the hashmap
+        return strategies[current_strategy](args...);
+    }
 
-        // void set_strategy(string name) {
-        //     this.name = name;
-        // }
-
-        // void execute() {
-        //     if name not in map {
-        //         return first entry
-        //     }
-
-        //     return hashmap[name]();
-        // }
-
-        // void execute_by_name(string strategy_name) {
-        //     // Call the functor from the hashmap
-        //     return hashmap[strategy_name]();
-        // }    
-// 
-// }
+    ReturnType execute_by_name(key strategy_name, Args... args)
+    {
+        return strategies[strategy_name](args...);
+    }
+};
